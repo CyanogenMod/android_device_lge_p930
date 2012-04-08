99d881a9e4621ee51fc91c02149bb5e259bb19c0
Ricardo Cerqueira <github@cerqueira.org>
2012-04-08 22:41:33 +0100

    p930: Adjust config to use qcom's CpuGovernorService
    We recently imported this into CM as part of the CAF OMX merge, so
    let's try to use it here...

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -46,14 +46,12 @@ case "$target" in
 # cpufreq ondemand gov
 	 echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 	 echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-	 echo 50000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-	 echo 50000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-	 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
-	 echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
+	 echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+	 echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-	 chown system /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-	 chown system /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
+	 chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
 #	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 #	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
