70ecfd6fbf5e4c22680061daa9cd904f1062f726
Ricardo Cerqueira <github@cerqueira.org>
2012-04-14 13:52:55 +0100

    p930: Enable all the USB compositions
    With the recent merge of UMS/MTP/PTP coexistence into the CM core,
    this should work fine...

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -90,3 +90,12 @@ case "$target" in
         start thermald
     ;;
 esac
+
+usb_config=`getprop persist.sys.usb.config`
+case "$usb_config" in
+    "") #USB persist config not set, select default configuration
+        setprop persist.sys.usb.config mtp,adb
+    ;;
+esac
+
+
