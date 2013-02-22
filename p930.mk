c604b717164920d4cf4b2ccd3f61924494915315
Tom Marshall <tdm.code@gmail.com>
2013-02-22 16:49:08 +0000

    [2/3] p930: Make init script, fstab, and i_*.kl model specific items.
    Model specific versions of init.iprj.rc, fstab.iprj, i_atnt-keypad.kl.
    
    Change-Id: Ic656eb460f75ba35ccf514cb22095002f41ac7a5
    
    Get rid of keypad layout, it is not needed.
    
    Change-Id: Ic656eb460f75ba35ccf514cb22095002f41ac7a5

--- p930.mk
+++ p930.mk
@@ -4,7 +4,12 @@ $(call inherit-product-if-exists, vendor/lge/p930/p930-vendor.mk)
 
 ## These are different between models
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal 
+    $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(LOCAL_PATH)/fstab.iprj:root/fstab.iprj
+
+## Model specific files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.iprj.rc:root/init.iprj.rc
 
 PRODUCT_NAME := full_p930 
 PRODUCT_DEVICE := p930 
