d576b04151835a46b69c401332072b86c25c065d
WinSuk <goodboy735@gmail.com>
2013-09-24 19:17:28 -0700

    p930: Use new method of picking boot animation
    Change-Id: I89a38b217429c64f7c8ed0aa756eb66b0c65960c

--- cm.mk
+++ cm.mk
@@ -1,7 +1,9 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
